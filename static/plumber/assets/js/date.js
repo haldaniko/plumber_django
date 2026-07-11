2d2a0e65303cfb2052dbcf369e9cfea016219553
Haldaniko <haldaniko@gmail.com>
2026-07-11 21:55:15 +0300

    Add team page template with team member profiles and footer details

---
+++ static/plumber/assets/js/date.js
@@ -0,0 +1,4 @@
+  // JavaScript to update the year
+  const copyrightYearElement = document.getElementById("copyright-year");
+  const currentYear = new Date().getFullYear();
+  copyrightYearElement.textContent = currentYear;
